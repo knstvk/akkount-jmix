0f176c7bf177a8c9b72f6b83370bc1644ecf8409
Konstantin Krivopustov <krivopustov@haulmont.com>
2021-02-22 09:40:37 +0400

    Add React frontend

---
+++ frontend/src/app/header/AppHeader.tsx
@@ -0,0 +1,50 @@
+import { LogoutOutlined } from "@ant-design/icons";
+import { Button, Modal } from "antd";
+import * as React from "react";
+import { observer } from "mobx-react";
+import "./AppHeader.css";
+import { injectMainStore, MainStoreInjected } from "@haulmont/jmix-react-core";
+import { LanguageSwitcher } from "../../i18n/LanguageSwitcher";
+import { injectIntl, WrappedComponentProps } from "react-intl";
+import JmixLightIcon from "../icons/JmixLightIcon";
+
+@injectMainStore
+@observer
+class AppHeader extends React.Component<
+  MainStoreInjected & WrappedComponentProps
+> {
+  render() {
+    const appState = this.props.mainStore!;
+
+    return (
+      <div className="app-header">
+        <JmixLightIcon className="app-header__icon" />
+
+        <div className="user-panel">
+          <LanguageSwitcher className="panelelement language-switcher -header" />
+          <span className="panelelement">{appState.userName}</span>
+          <Button
+            className="panelelement"
+            id="button_logout"
+            ghost={true}
+            icon={<LogoutOutlined />}
+            onClick={this.showLogoutConfirm}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  showLogoutConfirm = () => {
+    Modal.confirm({
+      title: this.props.intl.formatMessage({ id: "header.logout.areYouSure" }),
+      okText: this.props.intl.formatMessage({ id: "header.logout.ok" }),
+      cancelText: this.props.intl.formatMessage({ id: "header.logout.cancel" }),
+      onOk: () => {
+        this.props.mainStore!.logout();
+      }
+    });
+  };
+}
+
+export default injectIntl(AppHeader);
---
+++ frontend/src/jmix/entities/akk_CategoryBalance.ts
@@ -0,0 +1,5 @@
+export class CategoryAmount {
+  static NAME = "akk_CategoryBalance";
+}
+export type CategoryAmountViewName = "_base" | "_instance_name" | "_local";
+export type CategoryAmountView<V extends CategoryAmountViewName> = never;
