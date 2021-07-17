e44239144df86f173bb4a9df9f3d31843af2840e
Konstantin Krivopustov <krivopustov@haulmont.com>
2021-07-17 13:07:32 +0400

    RA: categories done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,15 @@ import * as React from 'react'
 import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin'
 import { jmixAuthProvider } from './jmix-ra/jmixAuthProvider'
 import { jmixDataProvider } from './jmix-ra/jmixDataProvider'
-// import { FooCreate, FooEdit, FooList } from './foo'
-// import { BarList } from "./bar"
+import { CategoryList, CategoryEdit, CategoryCreate } from "./category"
 
 const App = () => {
-  return (
-      <Admin dataProvider={jmixDataProvider} authProvider={jmixAuthProvider}>
-        <Resource name="akk_User" list={ListGuesser} />
-        {/*<Resource name="Foo-api" options={{label: "Foo"}} list={FooList} edit={FooEdit} create={FooCreate} />*/}
-        {/*<Resource name="Bar" list={BarList} />*/}
-      </Admin>
-  )
+    return (
+        <Admin dataProvider={jmixDataProvider} authProvider={jmixAuthProvider}>
+            <Resource name="akk_User" list={ListGuesser} />
+            <Resource name="akk_Category" options={{label: "Category"}} list={CategoryList} edit={CategoryEdit} create={CategoryCreate}/>
+        </Admin>
+    )
 }
 
 export default App;
---
+++ frontend/src/category.js
@@ -0,0 +1,39 @@
+import React from "react"
+import {
+    List, Datagrid, TextField, SelectField,
+    Edit, Create, SimpleForm, TextInput, SelectInput
+} from "react-admin"
+
+const categoryTypes = [
+    {id: "EXPENSE", name: "Expense"},
+    {id: "INCOME", name: "Income"}
+]
+
+export const CategoryList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <TextField source="id" />
+            <TextField source="name" />
+            <SelectField source="catType" choices={categoryTypes} />
+        </Datagrid>
+    </List>
+)
+
+export const CategoryEdit = props => (
+    <Edit {...props}>
+        <CategoryForm />
+    </Edit>
+)
+
+export const CategoryCreate = props => (
+    <Create {...props}>
+        <CategoryForm />
+    </Create>
+)
+
+const CategoryForm = props => (
+    <SimpleForm {...props} redirect="list">
+        <TextInput source="name"/>
+        <SelectInput source="catType" choices={categoryTypes}/>
+    </SimpleForm>
+)
