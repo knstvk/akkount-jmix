98d6500b137fb56c30ee8249b4979818d6cbc031
Konstantin Krivopustov <krivopustov@haulmont.com>
2021-07-17 14:27:54 +0400

    RA: accounts done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,16 @@ import { jmixAuthProvider } from './jmix-ra/jmixAuthProvider'
 import { jmixDataProvider } from './jmix-ra/jmixDataProvider'
 import { CategoryList, CategoryEdit, CategoryCreate } from "./category"
 import { CurrencyCreate, CurrencyEdit, CurrencyList } from "./currency"
+import { AccountCreate, AccountEdit, AccountList } from "./account"
 
 
 const App = () => {
     return (
         <Admin dataProvider={jmixDataProvider} authProvider={jmixAuthProvider}>
-            <Resource name="akk_User" list={ListGuesser} />
+            <Resource name="akk_Account-api" options={{label: "Account"}} list={AccountList} edit={AccountEdit} create={AccountCreate}/>
             <Resource name="akk_Category" options={{label: "Category"}} list={CategoryList} edit={CategoryEdit} create={CategoryCreate}/>
             <Resource name="akk_Currency" options={{label: "Currency"}} list={CurrencyList} edit={CurrencyEdit} create={CurrencyCreate}/>
+            <Resource name="akk_User" list={ListGuesser} />
         </Admin>
     )
 }
---
+++ frontend/src/account.js
@@ -0,0 +1,41 @@
+import React from "react"
+import {
+    List, Datagrid, TextField, BooleanField, NumberField,
+    Edit, Create, SimpleForm, TextInput, BooleanInput, NumberInput, ReferenceInput, SelectInput
+} from "react-admin"
+
+export const AccountList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <TextField source="name" />
+            <TextField source="description" />
+            <TextField source="currency.code" label="Currency" />
+            <NumberField source="group" />
+            <BooleanField source="active" />
+        </Datagrid>
+    </List>
+)
+
+export const AccountEdit = props => (
+    <Edit {...props}>
+        <AccountForm />
+    </Edit>
+)
+
+export const AccountCreate = props => (
+    <Create {...props}>
+        <AccountForm />
+    </Create>
+)
+
+const AccountForm = props => (
+    <SimpleForm {...props} redirect="list">
+        <TextInput source="name" />
+        <TextInput source="description" />
+        <ReferenceInput source="currency.id" reference="akk_Currency" label="Currency">
+            <SelectInput optionText="code" />
+        </ReferenceInput>
+        <NumberInput source="group" />
+        <BooleanInput source="active" />
+    </SimpleForm>
+)
