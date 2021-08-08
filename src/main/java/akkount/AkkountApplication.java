d2680a74aadf4a29848b5981dc26cf45156400bd
Konstantin Krivopustov <krivopustov@haulmont.com>
2021-08-08 21:59:12 +0400

    RA: building into bootJar

--- src/main/java/akkount/AkkountApplication.java
+++ src/main/java/akkount/AkkountApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.sql.DataSource;
 
@@ -21,5 +23,15 @@ public class AkkountApplication {
 	@ConfigurationProperties(prefix="main.datasource")
 	DataSource dataSource() {
 		return DataSourceBuilder.create().build();
+	}
+
+	@Bean
+	public WebMvcConfigurer forwardFrontendToIndex() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addViewControllers(ViewControllerRegistry registry) {
+				registry.addRedirectViewController("/front", "/front/index.html");
+			}
+		};
 	}
 }
