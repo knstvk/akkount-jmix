faf9043fb75f8892ed595ed5272c8aa2d7aabd90
Konstantin Krivopustov <krivopustov@haulmont.com>
2021-08-15 18:37:43 +0400

    Start/stop scripts and properties for prod env

---
+++ etc/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export JMIX_UI_PRODUCTIONMODE="true"
+export JMIX_UI_LOGIN_DEFAULTUSERNAME="<disabled>"
+export JMIX_UI_LOGIN_DEFAULTPASSWORD="<disabled>"
+
+java -jar akkount-0.3-SNAPSHOT.jar & echo $! > ./pid.file &
---
+++ etc/start_silent.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nohup ./start.sh > /dev/null 2> error.log < /dev/null &
