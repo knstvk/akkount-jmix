8b79e6049447974f9d6f663ba6fab30c46077bd0
Konstantin Krivopustov <krivopustov@haulmont.com>
2022-07-16 15:00:12 +0400

    Fix datasource definition

--- src/main/java/akkount/AkkountApplication.java
+++ src/main/java/akkount/AkkountApplication.java
@@ -1,14 +1,20 @@
 package akkount;
 
+import com.google.common.base.Strings;
+import org.apache.commons.lang3.SystemUtils;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,6 +23,9 @@ import javax.sql.DataSource;
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class AkkountApplication extends SpringBootServletInitializer {
+
+	@Autowired
+	private Environment environment;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AkkountApplication.class, args);
@@ -44,5 +53,15 @@ public class AkkountApplication extends SpringBootServletInitializer {
 				registry.addRedirectViewController("/front", "/front/index.html");
 			}
 		};
+	}
+
+	@EventListener
+	public void printApplicationUrl(ApplicationStartedEvent event) {
+		LoggerFactory.getLogger(AkkountApplication.class).info("{user.dir} is "	+ SystemUtils.getUserDir());
+
+		LoggerFactory.getLogger(AkkountApplication.class).info("Application started at "
+				+ "http://localhost:"
+				+ environment.getProperty("local.server.port")
+				+ Strings.nullToEmpty(environment.getProperty("server.servlet.context-path")));
 	}
 }
