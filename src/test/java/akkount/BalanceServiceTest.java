51ec6b13e370cd751db0c5f4b0cada9b99e7eed5
Konstantin Krivopustov <krivopustov@haulmont.com>
2026-02-07 21:42:57 +0400

    Run tests on a separate HSQL DB

--- src/test/java/akkount/BalanceServiceTest.java
+++ src/test/java/akkount/BalanceServiceTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
 
@@ -28,6 +29,7 @@ import static org.springframework.http.HttpMethod.GET;
 
 @SpringBootTest
 @ExtendWith(AuthenticatedAsAdmin.class)
+@ActiveProfiles("test")
 public class BalanceServiceTest {
 
     UUID account1Id;
--- src/test/java/akkount/CategoryGuesserTest.java
+++ src/test/java/akkount/CategoryGuesserTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
 
@@ -17,6 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ExtendWith(AuthenticatedAsAdmin.class)
+@ActiveProfiles("test")
 public class CategoryGuesserTest {
 
     @Autowired
